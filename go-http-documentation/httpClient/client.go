621af4e3e13a389791b84f2cc1bf0f024ff0aa24
Tomlee Abila <tomleeabila@gmail.com>
2024-07-09 22:27:26 +0300

    closing the response body after using it

--- go-http-documentation/httpClient/client.go
+++ go-http-documentation/httpClient/client.go
@@ -2,21 +2,35 @@ package httpclient
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
 func GoClient(){
-	resp, err := http.Get("https://reqres.in/")
-
-	if err != nil{
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println(resp)
+	resp, err := http.Get("https://reqres.in")
 
 	// resp, err := http.Post("http://example.com/upload", "image/jpeg", &buf)
 
 	// resp, err := http.PostForm("http://example.com/form",
 	// 	url.Values{"key": {"Value"}, "id": {"123"}})
+
+	if err != nil{
+		fmt.Println("Error!\n\n",err)
+		return
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	
+	if err != nil{
+		fmt.Println("Error!\n\n",err)
+		return
+	}
+
+	os.WriteFile("index.html", body, 0064)
+
+	fmt.Println("Status OK!!\n\n",string(body))
+
+	
 }
