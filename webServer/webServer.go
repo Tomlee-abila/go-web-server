52a9401125f76857f560720fb79c92522f2c5901
tabila <sheillajuma001@gmail.com>
2024-07-09 20:58:36 +0300

    Starting a server

--- webServer/webServer.go
+++ webServer/webServer.go
@@ -6,35 +6,9 @@ import (
 	"time"
 )
 
-var indexPage = `<!DOCTYPE html>
-<html>
-	<body>
-		<h1>My First Heading </h1>
-		<p>My First paragraph. </p>
-	</body>
-</html>
-`
-
-var userInfo = `{
-	"name": "testuser",
-	"age": 21
-}`
-
 func WebServer() {
 	address := ":8080"
 	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("content-type", "text/html")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(indexPage))
-	})
-
-	mux.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("content-type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(userInfo))
-	})
 
 	s := &http.Server{
 		Addr:           address,
